33bf931e649c9f72276ddd4d9d10f5288b506c0c
jlaunit89 <75558272+jlaunit89@users.noreply.github.com>
2021-07-17 06:51:42 -0700

    adding updated code

--- js/app.js
+++ js/app.js
@@ -10,46 +10,46 @@ let answerOne = prompt('Am I a Project Manager? Answer \'yes\'or\'no\'.').toLowe
 if(answerOne === 'yes' || answerOne === 'y') {
     alert('That is correct. I am a Project Manager!');
 }else if (answerOne === 'no' || answerOne === 'n') {
-    alert('Sorry, You are wrong.')
+    alert('Sorry, You are wrong.');
 }else {
-    alert('Sorry, plese answer with a yes or no')
+    alert('Sorry, plese answer with a yes or no');
 }
 
-let answerOne = prompt('Was I in the Army? Answer \'yes\'or\'no\'.').toLowerCase();
+let answerTwo = prompt('Was I in the Army? Answer \'yes\'or\'no\'.').toLowerCase();
 
-if(answerOne === 'yes' || answerOne === 'y') {
+if(answerTwo === 'yes' || answerTwo === 'y') {
     alert('That is correct. I was in the Army!');
-}else if (answerOne === 'no' || answerOne === 'n') {
-    alert('Sorry, You are wrong.')
+}else if (answerTwo === 'no' || answerTwo === 'n') {
+    alert('Sorry, You are wrong.');
 }else {
-    alert('Sorry, plese answer with a yes or no')
+    alert('Sorry, plese answer with a yes or no');
 }
 
-let answerOne = prompt('Do I live in Bellevue? Answer \'yes\'or\'no\'.').toLowerCase();
+let answerThree = prompt('Do I live in Bellevue? Answer \'yes\'or\'no\'.').toLowerCase();
 
-if(answerOne === 'yes' || answerOne === 'y') {
+if(answerThree === 'yes' || answerThree === 'y') {
     alert('That is correct. I am an east-sider!');
-}else if (answerOne === 'no' || answerOne === 'n') {
-    alert('Sorry, You are wrong.')
+}else if (answerThree === 'no' || answerThree === 'n') {
+    alert('Sorry, You are wrong.');
 }else {
-    alert('Sorry, plese answer with a yes or no')
+    alert('Sorry, plese answer with a yes or no');
 }
 
-let answerOne = prompt('Am I a Patriots fan? Answer \'yes\'or\'no\'.').toLowerCase();
+let answerFour = prompt('Am I a Patriots fan? Answer \'yes\'or\'no\'.').toLowerCase();
 
-if(answerOne === 'yes' || answerOne === 'y') {
+if(answerFour === 'yes' || answerFour === 'y') {
     alert('That is correct. GO PATS!');
-}else if (answerOne === 'no' || answerOne === 'n') {
-    alert('Sorry, You are wrong.')
+}else if (answerFour === 'no' || answerFour === 'n') {
+    alert('Sorry, You are wrong.');
 }else {
-    alert('Sorry, plese answer with a yes or no')
+    alert('Sorry, plese answer with a yes or no');
 }
 
-let answerOne = prompt('Do I have a college degree? Answer \'yes\'or\'no\'.').toLowerCase();
+let answerFive = prompt('Do I have a college degree? Answer \'yes\'or\'no\'.').toLowerCase();
 
-if(answerOne === 'yes' || answerOne === 'y') {
+if(answerFive === 'yes' || answerFive === 'y') {
     alert('Sorry, that is incorrect.');
-}else if (answerOne === 'no' || answerOne === 'n') {
+}else if (answerFive === 'no' || answerFive === 'n') {
     alert('Correct. I do not have a college degree yet.')
 }else {
     alert('Sorry, plese answer with a yes or no')
